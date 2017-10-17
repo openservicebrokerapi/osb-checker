e271d287f490cd6a1e18008d56c4fb090a5be3eb
Haishi Bai <hbai@microsoft.com>
2017-10-17 10:21:36 -0700

    refactor & readme update

--- 2.13/mocks/mockOSB.js
+++ 2.13/mocks/mockOSB.js
@@ -9,136 +9,14 @@ var validator = new Validator();
 var provisionRequestSchema = require('./schemas/provision_request.json');
 var updateRequestSchema = require('./schemas/update_request.json');
 var bindingRequestSchema = require('./schemas/binding_request.json');
+var serviceCatalog = require('./data/service_catalog.json');
 
 //Catalog Management
 //- GET /v2/catalog
 app.get('/v2/catalog', function (req,res) {
   if (!checkRequest(req,res))
     return;
-    res.send(
-        {
-            "services": [{
-              "name": "fake-service",
-              "id": "acb56d7c-XXXX-XXXX-XXXX-feb140a59a66",
-              "description": "fake service",
-              "tags": ["no-sql", "relational"],
-              "requires": ["route_forwarding"],
-              "bindable": true,
-              "metadata": {
-                "provider": {
-                  "name": "The name"
-                },
-                "listing": {
-                  "imageUrl": "http://example.com/cat.gif",
-                  "blurb": "Add a blurb here",
-                  "longDescription": "A long time ago, in a galaxy far far away..."
-                },
-                "displayName": "The Fake Broker"
-              },
-              "dashboard_client": {
-                "id": "398e2f8e-XXXX-XXXX-XXXX-19a71ecbcf64",
-                "secret": "277cabb0-XXXX-XXXX-XXXX-7822c0a90e5d",
-                "redirect_uri": "http://localhost:1234"
-              },
-              "plan_updateable": true,
-              "plans": [{
-                "name": "fake-plan-1",
-                "id": "d3031751-XXXX-XXXX-XXXX-a42377d3320e",
-                "description": "Shared fake Server, 5tb persistent disk, 40 max concurrent connections",
-                "free": false,
-                "metadata": {
-                  "max_storage_tb": 5,
-                  "costs":[
-                      {
-                         "amount":{
-                            "usd":99.0
-                         },
-                         "unit":"MONTHLY"
-                      },
-                      {
-                         "amount":{
-                            "usd":0.99
-                         },
-                         "unit":"1GB of messages over 20GB"
-                      }
-                   ],
-                  "bullets": [
-                    "Shared fake server",
-                    "5 TB storage",
-                    "40 concurrent connections"
-                  ]
-                },
-                "schemas": {
-                  "service_instance": {
-                    "create": {
-                      "parameters": {
-                        "$schema": "http://json-schema.org/draft-04/schema#",
-                        "type": "object",
-                        "properties": {
-                          "billing-account": {
-                            "description": "Billing account number used to charge use of shared fake server.",
-                            "type": "string"
-                          }
-                        }
-                      }
-                    },
-                    "update": {
-                      "parameters": {
-                        "$schema": "http://json-schema.org/draft-04/schema#",
-                        "type": "object",
-                        "properties": {
-                          "billing-account": {
-                            "description": "Billing account number used to charge use of shared fake server.",
-                            "type": "string"
-                          }
-                        }
-                      }
-                    }
-                  },
-                  "service_binding": {
-                    "create": {
-                      "parameters": {
-                        "$schema": "http://json-schema.org/draft-04/schema#",
-                        "type": "object",
-                        "properties": {
-                          "billing-account": {
-                            "description": "Billing account number used to charge use of shared fake server.",
-                            "type": "string"
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }, {
-                "name": "fake-plan-2",
-                "id": "0f4008b5-XXXX-XXXX-XXXX-dace631cd648",
-                "description": "Shared fake Server, 5tb persistent disk, 40 max concurrent connections. 100 async",
-                "free": false,
-                "metadata": {
-                  "max_storage_tb": 5,
-                  "costs":[
-                      {
-                         "amount":{
-                            "usd":199.0
-                         },
-                         "unit":"MONTHLY"
-                      },
-                      {
-                         "amount":{
-                            "usd":0.99
-                         },
-                         "unit":"1GB of messages over 20GB"
-                      }
-                   ],
-                  "bullets": [
-                    "40 concurrent connections"
-                  ]
-                }
-              }]
-            }]
-          }
-    )
+  res.send(serviceCatalog);
 })
 app.put('/v2/service_instances/:instance_id', function (req,res) {  
   if (!checkRequest(req,res))
@@ -267,11 +145,12 @@ function checkRequest(req,res) {
     var parts = auth.split(/:/);
     username = parts[0];
     password = parts[1];
-  }    
-  if (!req.header('X-Broker-API-Version') || req.header('X-Broker-API-Version') != '2.13') {
+  }      
+  if (!req.header('X-Broker-API-Version') || req.header('X-Broker-API-Version') != '2.13') {    
     res.sendStatus(412);
     return false;
   }
+  
   if (!username || !password || username != 'username' || password != 'password') {
     res.sendStatus(401);
     return false;
--- README.md
+++ README.md
@@ -7,13 +7,13 @@ An automatic checker to verify an Open Service Broker API implementation against
 2. Modify test configurations. By default, test parameters are loaded from the **test\config_default.json** file. You need to update the **url**, **user**, and **password** fields to match with your OSB endpoint. 
 ```json
     "url": "<your OSB endpoint>",
-    "apiVersion" : "2.1.3",
+    "apiVersion" : "2.13",
     "user": "<your user name>",
     "password": "<your password>",
 ```
 3. Run tests
 ```bash
-    cd c:\osb-checker\2.1.3\tests
+    cd c:\osb-checker\2.13\tests
     npm install
     mocha
 ```
@@ -22,3 +22,11 @@ An automatic checker to verify an Open Service Broker API implementation against
 
 ### Provisioning requests
 Provisioning requests are configured by the **provisions** array in your test configuration file. You can modify the array to use different *service_ids* and *plan_ids*. You can specify *instnace_id*. If you leave the field as an empty string, a random instance id will be used during tests.
+
+## Mock OSB API Endpoint
+OSB Checker also comes with a mock OSB API server that can be used to test marketplace implementations. To launch the Mock server:
+```bash
+    cd c:\osb-checker\2.13\mocks
+    npm install
+    node mockOSB.js
+```
